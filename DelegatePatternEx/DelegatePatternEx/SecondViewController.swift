ec8cf9713dd85976d4399ab68991c6c2be553946
zooneon <snsxhdz@naver.com>
2021-01-03 23:42:39 +0900

    SecondViewController, 프로토콜 작성

--- DelegatePatternEx/DelegatePatternEx/SecondViewController.swift
+++ DelegatePatternEx/DelegatePatternEx/SecondViewController.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+protocol ChangeTextDelegate {
+    func changeText()
+}
 
+class SecondViewController: UIViewController {
+    
+    var delegate: ChangeTextDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func clickButtonTapped(_ sender: UIButton) {
+        self.delegate?.changeText()
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
