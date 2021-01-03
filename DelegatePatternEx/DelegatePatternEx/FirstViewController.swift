4382a6fe13a8af5885c3578da792ca39da400612
zooneon <snsxhdz@naver.com>
2021-01-03 23:30:49 +0900

    FirstViewController 작성

--- DelegatePatternEx/DelegatePatternEx/FirstViewController.swift
+++ DelegatePatternEx/DelegatePatternEx/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  DelegatePatternEx
 //
 //  Created by 권준원 on 2021/01/03.
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else { return }
+        
+        vc.modalTransitionStyle = .coverVertical
+        self.present(vc, animated: true, completion: nil)
+    }
+    
 }
 
